3c30745fd4f784f2d8b5e995fcc44944920e483f
elenmari <elenamaria1292@gmail.com>
2024-01-16 11:24:39 +1300

    chore: initial commit.

---
+++ app.py
@@ -0,0 +1,16 @@
+
+import sys
+
+
+def main(pdf_path):
+
+    return
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <pdf_path>")
+        sys.exit(1)
+
+    pdf_path = sys.argv[1]
+    main(pdf_path)
